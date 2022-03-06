7c7d272627af9dda83e0340925c56e54acb4dc07
Vinayak <vinayaksawant859@gmail.com>
2022-03-06 16:11:53 +0530

    Splash screen animated

--- app/src/main/java/com/example/lilcare/MainActivity.java
+++ app/src/main/java/com/example/lilcare/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.lilcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView bgslide;
+    Animation bganim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bgslide = (ImageView) findViewById(R.id.bgslide);
+
+        bganim = AnimationUtils.loadAnimation(this, R.anim.bganim);
+
+        bgslide.animate().translationY(-1500).setDuration(800).setStartDelay(300);
+
     }
 }
