82fbb5961a088d43a477fef10d409f2ae5229db1
im-Jake <78202520+im-Jake@users.noreply.github.com>
2022-05-01 17:01:17 +0530

    Added About Us
    - Added about page
    The About Us Page shows the description of our team members
    - It also shows the app icon and app version
    Link to the project on GitHub will be added later

--- app/src/main/java/com/example/lilcare/AboutUs.java
+++ app/src/main/java/com/example/lilcare/AboutUs.java
@@ -11,6 +11,7 @@ public class AboutUs extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
 
     }
 
