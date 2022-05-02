9b6687108e0b037c88286888f4c4aad290c2842d
Vinayak <vinayaksawant859@gmail.com>
2022-05-02 22:55:39 +0530

    fixed forced Dark Mode

--- app/src/main/java/com/example/lilcare/MainActivity.java
+++ app/src/main/java/com/example/lilcare/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         Parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, parentRegPage.class);
+                Intent i = new Intent(MainActivity.this, parentLogPage.class);
                 startActivity(i);
                 //hii
             }
